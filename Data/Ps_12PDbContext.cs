de2185fcd6b5d1e89155bef7fc3fc5ccc27c22a2
Arav105 <chachuarav@gmail.com>
2023-08-28 16:42:46 +0530

    Ps-12

---
+++ Data/Ps_12PDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Ps_12.Data
+{
+    public class Ps_12PDbContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Ps_12PDbContext() : base("name=Ps_12PDbContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Ps_12.Models.Comment> Comments { get; set; }
+
+        public System.Data.Entity.DbSet<Ps_12.Models.Post> Posts { get; set; }
+    }
+}
